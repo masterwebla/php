fb26b79e4aafc82ba1fa6cd708a0c2702999e0fe
Mauricio Mac <mac@masterweb.la>
2019-04-04 20:16:15 -0500

    Listado con Bootstrap

--- listar.php
+++ listar.php
@@ -11,26 +11,42 @@
 <html>
 <head>
 	<title>Listar Usuarios</title>
+	<meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<h1>Listado de Usuarios</h1>
-	<table width="100%" border="1">
-		<tr>
-			<th>Nombres</th>
-			<th>Email</th>
-			<th>Edad</th>
-			<th>Ciudad</th>
-			<th>Opciones</th>
-		</tr>
-		<?php while($fila = $resultado->fetch(PDO::FETCH_ASSOC)){ ?>
-			<tr>
-				<td><?php echo $fila['nombres']; ?></td>
-				<td><?php echo $fila['email']; ?></td>
-				<td><?php echo $fila['edad']; ?></td>
-				<td><?php echo $fila['ciudad']; ?></td>
-				<td></td>
-			</tr>
-		<?php } ?>
-	</table>
+	<div class="container">
+		<h1 class="text-center">Listado de Usuarios <a class="btn btn-success" href="registro.php"><i class="fas fa-plus"></i></a></h1>
+		<table class="table table-hover table-striped">
+			<thead>
+				<tr>
+					<th>Nombres</th>
+					<th>Email</th>
+					<th>Edad</th>
+					<th>Ciudad</th>
+					<th>Opciones</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php while($fila = $resultado->fetch(PDO::FETCH_ASSOC)){ ?>
+					<tr>
+						<td><?php echo $fila['nombres']; ?></td>
+						<td><?php echo $fila['email']; ?></td>
+						<td><?php echo $fila['edad']; ?></td>
+						<td><?php echo $fila['ciudad']; ?></td>
+						<td>
+							<a class="btn btn-info" href="#"><i class="fas fa-edit"></i></a>
+							<a class="btn btn-danger" href="#"><i class="fas fa-trash"></i></a>
+						</td>
+					</tr>
+				<?php } ?>
+			</tbody>	
+		</table>
+	</div>	
 </body>
 </html>
